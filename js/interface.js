fa879827d941f09c5e08372ef0c8b7f149acaa3b
Stephen Lloyd <stephen@Stephens-MacBook-Pro-2.local>
2015-03-06 16:13:56 +0000

    updata js

--- js/interface.js
+++ js/interface.js
@@ -3,17 +3,7 @@ $(document).ready(function(){
   var citiesApiUrl = "http://weather-api.herokuapp.com/cities";
   var cityApiUrl = "http://weather-api.herokuapp.com/temperature?city=";
 
-  $.ajax({
-    url: citiesApiUrl,
-    dataType:'jsonp',
-  }).done(function(data) {
-      $.each(data.cities, function(index, city) {
-        $('.cities').append(
-          $('<option></option>').val(city).html(city + " ⌄"));
-        });
-      });
-
-  $('.cities').change(function(){
+  var populateTempData = function() {
     var city = $( "select option:selected" ).val();
     $.ajax({
       url: cityApiUrl + city,
@@ -21,8 +11,21 @@ $(document).ready(function(){
     }).done(function(data) {
       $('.temperature').text(data.temp);
       $('img').attr('src', 'images/' + data.outlook + '.svg')
-      $( ".icon" ).removeClass().addClass("icon " + data.outlook);
+      $( "#icon" ).attr("class", data.outlook);
     });
+  };
+
+  $.ajax({url: citiesApiUrl, dataType:'jsonp',
+  }).done(function(data) {
+      $.each(data.cities, function(index, city) {
+        $('.cities').append(
+          $('<option></option>').val(city).html(city + " ⌄"));
+            populateTempData();
+        });
+      });
+
+  $('.cities').change(function(){
+    populateTempData();
   });
 
 });
