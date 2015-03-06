7aba4f55d33319f7a2c54be312f2e88c5e8c890a
Stephen Lloyd <stephen@Stephens-MacBook-Pro-2.local>
2015-03-06 16:26:01 +0000

    refactored js

--- js/interface.js
+++ js/interface.js
@@ -1,31 +1,29 @@
 $(document).ready(function(){
-
   var citiesApiUrl = "http://weather-api.herokuapp.com/cities";
   var cityApiUrl = "http://weather-api.herokuapp.com/temperature?city=";
 
   var populateTempData = function() {
     var city = $( "select option:selected" ).val();
-    $.ajax({
-      url: cityApiUrl + city,
-      dataType: 'jsonp'
-    }).done(function(data) {
-      $('.temperature').text(data.temp);
-      $('img').attr('src', 'images/' + data.outlook + '.svg')
-      $( "#icon" ).attr("class", data.outlook);
-    });
+    $.ajax({ url: cityApiUrl + city, dataType: 'jsonp'
+    }).done(function(data){setPageData(data);});
   };
 
+  var setPageData = function(data){
+    $('.temperature').text(data.temp);
+    $('img').attr('src', 'images/' + data.outlook + '.svg');
+    $( "#icon" ).attr("class", data.outlook);
+  };
+
+  var setDropdown = function(city) {
+    $('.cities').append(
+      $('<option></option>').val(city).html(city + " ⌄"));
+      populateTempData();
+    };
+
   $.ajax({url: citiesApiUrl, dataType:'jsonp',
-  }).done(function(data) {
-      $.each(data.cities, function(index, city) {
-        $('.cities').append(
-          $('<option></option>').val(city).html(city + " ⌄"));
-            populateTempData();
-        });
-      });
+    }).done(function(data) {
+      $.each(data.cities, function(index, city) {setDropdown(city);});
+    });
 
-  $('.cities').change(function(){
-    populateTempData();
-  });
-
+  $('.cities').change(function(){populateTempData();});
 });
